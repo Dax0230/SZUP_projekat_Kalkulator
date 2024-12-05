36edaa113c6e76464f6251d2394574a8fc79fa99
Dax0230 <davidnadoba1@gmail.com>
2024-12-05 22:40:48 +0100

    Unos izbora operatora

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 
 int main() { 
 double br1, br2, rezultat;
+
+    char operator;
+    printf("Unesite operaciju (+, -, *, /): ");
+    scanf(" %c", &operator);
+    
     printf("Unesite dva broja: ");
     scanf("%lf %lf", &br1, &br2);
   
